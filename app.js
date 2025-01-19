57eaca1e063afa6329b6472cb0215e87c1c771b2
Samanvith <Samanvith2005@gmail.com>
2025-01-19 19:35:38 +0530

    InitialCommit

---
+++ app.js
@@ -0,0 +1,7 @@
+let name="Samanvith"
+ let MyAge=22
+
+ console.log(`My name is ${name} and my age is ${MyAge}`)
+ console.log("globalObject In NodeJs::",global)
+ // Another way of global object called used in all js engines
+    // console.log("globalObject In NodeJs::",globalThis)
